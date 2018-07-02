4038e0186586c81f6648584cc4e8979d2cae10df
Josiah Grace <josiah@opendoor.com>
2018-07-02 09:42:27 -0700

    Update webby to v0.2.3

--- Formula/webby.rb
+++ Formula/webby.rb
@@ -1,9 +1,9 @@
 class Webby < Formula
   desc "Webby is an opinionated web app generator for front-end projects at Opendoor."
   homepage "https://github.com/opendoor-labs/webby"
-  url "https://github.com/opendoor-labs/webby/releases/download/0.2.2/webby",
+  url "https://github.com/opendoor-labs/webby/releases/download/0.2.3/webby",
     :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "eed30fceb437e24a0db51d01b99a5d2adb3d7713c83239036e95fbf41f6cc872"
+  sha256 "21e26b4d69b61a7399a50a643ef3f1f50693a3b2865b11410ab384f3c51785d5"
 
   bottle :unneeded
 
