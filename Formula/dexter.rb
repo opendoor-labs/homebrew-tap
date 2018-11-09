8a83d9740e3b0d52ede2875a87961b45d73f26f5
Leo Chu <leo.chu@opendoor.com>
2018-11-09 15:32:23 -0800

    Update Dexter
    - Move repo to S3

--- Formula/dexter.rb
+++ Formula/dexter.rb
@@ -1,7 +1,7 @@
 class Dexter < Formula
   desc "A command line tool to login to Vault using Github"
   homepage "https://github.com/opendoor-labs/dexter"
-  url "https://github.com/opendoor-labs/dexter/files/2567751/dexter-0.5.0.tar.gz"
+  url "https://s3.amazonaws.com/opendoor-brew-tap-bin/dexter-0.5.0.tar.gz"
   sha256 "23e55298a3ba00f4d3352f056bf7f4eaa401e314413f0d7f28bfeac55330f69b"
   version "0.5.0"
 
