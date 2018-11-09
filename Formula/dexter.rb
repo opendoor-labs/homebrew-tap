54107a39e4bf29d6349f568cf7df9c9c7528265d
Leo Chu <leo.chu@opendoor.com>
2018-11-09 15:06:05 -0800

    Add Dexter

---
+++ Formula/dexter.rb
@@ -0,0 +1,15 @@
+class Dexter < Formula
+  desc "A command line tool to login to Vault using Github"
+  homepage "https://github.com/opendoor-labs/dexter"
+  url "https://github.com/opendoor-labs/dexter/files/2567751/dexter-0.5.0.tar.gz"
+  sha256 "23e55298a3ba00f4d3352f056bf7f4eaa401e314413f0d7f28bfeac55330f69b"
+  version "0.5.0"
+
+  depends_on "curl"
+
+  bottle :unneeded
+
+  def install
+    bin.install "dexter"
+  end
+end
