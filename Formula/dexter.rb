6e6d7d7085f6e117246d167a1de14a5f33b8b423
Leo Chu <leo.chu@opendoor.com>
2018-11-16 15:32:23 -0800

    Update Dexter

--- Formula/dexter.rb
+++ Formula/dexter.rb
@@ -1,9 +1,9 @@
 class Dexter < Formula
   desc "A command line tool to login to Vault using Github"
   homepage "https://github.com/opendoor-labs/dexter"
-  url "https://s3.amazonaws.com/opendoor-brew-tap-bin/dexter-0.5.0.tar.gz"
-  sha256 "23e55298a3ba00f4d3352f056bf7f4eaa401e314413f0d7f28bfeac55330f69b"
-  version "0.5.0"
+  url "https://s3.amazonaws.com/opendoor-brew-tap-bin/dexter-0.1.0.tar.gz"
+  sha256 "dd797bc1c818cd8d896318147c28183660c710902c8ba1d00f1df6aade5dd5dc"
+  version "0.1.0"
 
   depends_on "curl"
 
