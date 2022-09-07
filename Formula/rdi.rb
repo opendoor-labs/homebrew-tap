4519fc899fd046e6f8d251acaebf9db1ea1a042e
Brian Malehorn <brian.malehorn@opendoor.com>
2022-09-07 16:49:34 -0700

    rdi: add formula for remote development instances script

---
+++ Formula/rdi.rb
@@ -0,0 +1,16 @@
+class Rdi < Formula
+  desc "Command-line tool 'rdi' to create and manage Remote Development Instances"
+  homepage "https://github.com/opendoor-labs/rdi"
+  
+  url "git@github.com:opendoor-labs/rdi", using: :git, branch: "main"
+  version "0.0.1"
+
+  def install
+    libexec.install "rdi", "ec2_script.sh", "ssh_config"
+    bin.install_symlink libexec/"rdi"
+  end
+
+  test do
+    assert_match "usage: rdi", shell_output("./rdi --help")
+  end
+end
