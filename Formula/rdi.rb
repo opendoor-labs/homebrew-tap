c3f91649d93e6d4345e18fc8c884dace07eea7c4
Brian Malehorn <bmalehorn@users.noreply.github.com>
2022-09-14 12:16:37 -0700

    rdi: update to 0.0.4, adding rdi terminate (#40)

--- Formula/rdi.rb
+++ Formula/rdi.rb
@@ -2,8 +2,10 @@ class Rdi < Formula
   desc "Command-line tool 'rdi' to create and manage Remote Development Instances"
   homepage "https://github.com/opendoor-labs/rdi"
   
-  url "git@github.com:opendoor-labs/rdi", using: :git, branch: "main"
-  version "0.0.3"
+  url "git@github.com:opendoor-labs/rdi", 
+    using: :git,
+    revision: "2cb3014ad7dc91136a9338e73f4f3178f03ea707"
+  version "0.0.4"
   depends_on "awscli"
   depends_on "saml2aws"
   depends_on "jq"
