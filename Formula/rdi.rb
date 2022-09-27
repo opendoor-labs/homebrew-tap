bc8c4e2d6b1a79a7e8f2c0b750cb0f1c8da0976f
Brian Malehorn <brian.malehorn@opendoor.com>
2022-09-27 11:50:09 -0700

    rdi: upgrade to v0.0.8, fixing sessionmanager installation

--- Formula/rdi.rb
+++ Formula/rdi.rb
@@ -4,8 +4,8 @@ class Rdi < Formula
   
   url "git@github.com:opendoor-labs/rdi", 
     using: :git,
-    tag: "v0.0.7"
-  version "0.0.7"
+    tag: "v0.0.8"
+  version "0.0.8"
   depends_on "awscli"
   depends_on "saml2aws"
   depends_on "jq"
